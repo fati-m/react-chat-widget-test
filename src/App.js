9a25d5db5515c09a09266ddc38933c65e979f341
Fatimah <famohammed2@wisc.edu>
2023-06-06 23:05:46 -0500

    classname change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   };
 
   return (
-    <div className="App">
+    <div className="App chat-widget-container">
       <Widget
         handleNewUserMessage={handleNewUserMessage}
         profileAvatar={nostalLogo}
